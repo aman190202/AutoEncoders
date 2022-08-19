c40b8a9be221c4d79b0a1660ed23c2a27bcf9982
Aman Agarwal <agarwalaman190202@gmail.com>
2022-08-20 00:52:33 +0530

    DS store ignored

--- convert_to_grey.py
+++ convert_to_grey.py
@@ -2,15 +2,20 @@ from PIL import Image
 import os
 
 directory = 'Google_Recaptcha_V2_Images_Dataset/images/Chimney'
+directory_intended= 'Google_Recaptcha_V2_Images_Dataset/images/Chimney_grey'
+
 
 def convtogrey(directory,filename):
     fn = os.path.join(directory, filename)
-    img = Image.open('test.jpg')
+    img = Image.open(fn)
     imgGray = img.convert('L')
+    filename= filename + 'grey'
     imgGray.save('test_gray.jpg')
 
 
 for filename in os.listdir(directory):
+
+    os.mkdir()
     if os.path.isfile(directory,filename):
         convtogrey(directory,filename)
 
