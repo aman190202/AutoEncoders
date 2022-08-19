5d1b1a4259bb2866809a476951e08ecb418f6a23
Aman Agarwal <agarwalaman190202@gmail.com>
2022-08-20 01:13:14 +0530

    Conversion file changed

--- convert_to_grey.py
+++ convert_to_grey.py
@@ -3,19 +3,18 @@ import os
 
 directory = 'Google_Recaptcha_V2_Images_Dataset/images/Chimney'
 directory_intended= 'Google_Recaptcha_V2_Images_Dataset/images/Chimney_grey'
-
+os.mkdir(directory_intended)
 
 def convtogrey(directory,filename):
     fn = os.path.join(directory, filename)
     img = Image.open(fn)
     imgGray = img.convert('L')
-    filename= filename + 'grey'
-    imgGray.save('test_gray.jpg')
+    filename= os.path.join(directory_intended,filename)
+    imgGray.save(filename)
 
 
 for filename in os.listdir(directory):
-
-    os.mkdir()
-    if os.path.isfile(directory,filename):
-        convtogrey(directory,filename)
+    if filename == '.DS_Store' :
+        continue
+    convtogrey(directory,filename)
 
